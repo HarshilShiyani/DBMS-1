92a0f4dc3932ce19dfd4cf21201cd5b8decd8fee
Harshil Shiyani <95012680+harshilkumar@users.noreply.github.com>
2022-03-17 08:29:39 +0530

    Update Lab1-4(Dbms).sql

--- Lab1-4(Dbms).sql
+++ Lab1-4(Dbms).sql
@@ -188,5 +188,22 @@ select FirstNmae,LastName from Students123 where FirstNmae like 'k%'
 19  select stuid,city from Students123 where division like '______' and website is not null
 20 select * from Students123 where FirstNmae not like  '__[a,e,i,o,u]%'
  
+ lAB-6
+
+1. select 5*30
+2. select abs(-25),abs(25),abs(-50),abs(50)
+3. select ceiling(25.2),ceiling(25.7),ceiling(-25.2)
+4. select floor(25.2),floor(25.7),floor(-25.2)
+5. select 5%2,5%3
+6. select power(3,2),power(4,3)
+7. select sqrt(25),sqrt(30),sqrt(50)
+8. select square(5),square(15),square(25)
+9. select pi()
+10. select round(157.732,2),round(157.732,0),round(157.732,-2)
+11. select exp(2),exp(3) 
+12. select log10(5),log10(100)  
+13. select sin(3.1415),cos(3.1415),tan(3.1415)
+14. select sign(-25),sign(0),sign(25)
+15. select rand()
 
 
